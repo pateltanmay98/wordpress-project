7b241ed327c9d563977c6b64b1e29c5d1478eb7c
unknown <patelth1998@gmail.com>
2023-04-24 22:01:25 +0530

    Small changes

--- wp-content/themes/my-custom-theme/functions.php
+++ wp-content/themes/my-custom-theme/functions.php
@@ -18,6 +18,7 @@ function theme_features() {
   add_theme_support('post-thumbnails');
   add_image_size('professorLandscape', 400, 260, true);
   add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'theme_features');
@@ -47,3 +48,26 @@ function custom_adjust_queries($query) {
 }
 
 add_action('pre_get_posts', 'custom_adjust_queries');
+
+function pageBanner($args) {
+
+  if(!$args['photo']) {
+    if(get_field('page_banner_background_image')) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+
+ ?>
+ <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'] ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo (!$args['title']) ? get_the_title() : $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo (!$args['subtitle']) ? the_field('page_banner_subtitle') : $args['subtitle'] ?></p>
+      </div>
+    </div>  
+  </div>
+ <?php 
+}
